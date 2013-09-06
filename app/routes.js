e1e6cce66bca1be9a343ca84e8b74fcf6f298170
Hack Reactor Students <github@hackreactor.com>
2013-09-06 15:55:01 -0700

    Refactor per Ari

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,6 @@
 var passport      = require('passport');
 
 module.exports = function(app, config) {
-  // Setup CORS
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.clientUrl);
-    res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-
-  app.options('*', function(req, res){
-    res.send(200); 
-  });
-
   // Setup API blockade
   app.all('/api/*', function(req, res, next) {
     // passport gives us a 'isAuthenticated' method
--- config/db.js
+++ config/db.js
@@ -5,6 +5,16 @@ module.exports = function(app, config) {
   var db = mongoose.connect(config.db);
 
   // Setup database and UserSchema
+  var UserSchema = new Schema({
+    username: {type: 'string', index: {unique: true}},
+    hashed_password: 'string'
+  });
+
+  UserSchema.virtual('password').set(function(pass) {
+    this.hashed_password = pass;
+  });
+
+  mongoose.model('User', UserSchema);
 
   return db;
-}
+};
